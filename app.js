bb6e3f8f3bd91e050f9c0b08e3bc66ef59f35954
Sylvester Roos <sylvester.roos@xs4all.nl>
2022-12-13 13:51:06 +0100

    initial routing setup done

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
 require("dotenv").config();
+const cors = require("cors");
+const router = require("./src/routes/router");
+// const errors = require("./src/helpers/errors");
 
 const express = require("express");
 const app = express();
 
 // middleware
 app.use(express.json());
+app.use(cors());
 
-console.log("hello world");
+app.use("/api", router);
 
-// routes
-app.get("/", (req, res) => {
-  res.send(
-    "<h1>StatusMonitor Home Energy API</h1><a href='/api/v1/monitors'>test</a>"
-  );
+// error responses
+app.use("*", (err, req, res, next) => {
+  console.error(`${err.name}: ${err.message}`);
+  next(err);
+});
+
+// app.use("*", errors.handlers);
+
+app.use("*", (err, req, res, next) => {
+  if (err.code === "invalid_token") {
+    res.status(401).json({
+      error: "Token invalid",
+    });
+  } else {
+    res.status(500).json({
+      error: "something unexpected happened. please contact developers",
+    });
+  }
 });
 
 const port = process.env.PORT || 3333;
-
 const start = async () => {
   try {
-    app.listen(port, console.log(`Server listening on port ${port}...`));
+    app.listen(
+      port,
+      console.log(`Server listening on http://localhost:${port}/api/`)
+    );
   } catch (error) {
     console.log(error);
   }
--- routes/hello-world.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.route("/api").get((req, res) => {
-  res.send("hello world");
-});
-
-module.exports = router;
