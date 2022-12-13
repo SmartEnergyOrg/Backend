13d69fa94a8ec259fb8ff9e8d41e589ef6a2f1f6
Joost <ja.vandam3@student.avans.nl>
2022-12-13 12:11:12 +0100

    setup app.js
    npm installed express & dotenv

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('hello world');
+require("dotenv").config();
+
+// importeer express
+const express = require("express");
+const app = express();
+
+// middleware
+app.use(express.json());
+
+console.log("hello world");
+
+// routes
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>Statusmonitor Home Energy API</h1><a href='/api/v1/monitors'>test</a>"
+  );
+});
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    // connectDB
+    app.listen(port, console.log(`Server listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
