23712152fb64d799dc8984522ed0f040f396c4a7
Sylvester Roos <sylvester.roos@xs4all.nl>
2022-12-14 16:40:43 +0100

    minor edits

--- src/controllers/powersources/solar.controller.js
+++ src/controllers/powersources/solar.controller.js
@@ -1,5 +1,10 @@
 const solarService = require("../../services/powersources/solar.service");
 
+/**
+ * Calls the getCompleteTimeline function on the service. Returns the result as a http response
+ * @param req
+ * @param res
+ */
 const getCompleteTimeline = (req, res) => {
   const completeTimeline = solarService.getCompleteTimeline();
   res.status(200).json({ completeTimeline });
--- src/routes/powersources/powersources.routes.js
+++ src/routes/powersources/powersources.routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const solarController = require("../../controllers/powersources/solar.controller");
 
-router.get("", solarController.getCompleteTimeline);
+router.get("/solar", solarController.getCompleteTimeline);
 
 module.exports = router;
