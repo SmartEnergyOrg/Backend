55e6f5c6c0bd385c4bce76991491ceaa5dcb3c4f
Sylvester Roos <sylvester.roos@xs4all.nl>
2022-12-14 16:41:44 +0100

    created population script

---
+++ src/db/populate.js
@@ -0,0 +1,86 @@
+require("dotenv").config();
+
+const { InfluxDB, Point } = require("@influxdata/influxdb-client");
+const { DeleteAPI } = require("@influxdata/influxdb-client-apis");
+
+/** Environment variables **/
+const url = `${process.env.INFLUXDB_HOST}:${process.env.INFLUXDB_PORT}`;
+const token = process.env.INFLUXDB_TOKEN;
+const org = process.env.INFLUXDB_ORG;
+const bucket = process.env.INFLUXDB_BUCKET;
+
+console.log(`variables:
+url: ${url}
+token: ${token}
+org: ${org}
+bucket: ${bucket}\n`);
+
+/**
+ * Instantiate the InfluxDB client
+ * with a configuration object.
+ **/
+const influxDB = new InfluxDB({ url, token });
+
+/**
+ * create a variable holding the DeleteAPI
+ **/
+const deleteApi = new DeleteAPI(influxDB);
+
+/**
+ * This function will delete all the data from the database
+ */
+async function deleteAllData() {
+  // define time interval for delete operation
+  const stop = new Date();
+  const start = new Date(1970);
+
+  await deleteApi.postDelete({
+    org,
+    bucket,
+    body: {
+      start: start.toISOString(),
+      stop: stop.toISOString(),
+      predicate: '_measurement="solar"',
+    },
+  });
+}
+
+deleteAllData()
+  .then(() => {
+    console.log("Sucessfully deleted");
+
+    /**
+     * Create a write client from the getWriteApi method.
+     * Provide your `org` and `bucket`.
+     **/
+    const writeApi = influxDB.getWriteApi(org, bucket);
+
+    /**
+     * Create a point and write it to the buffer.
+     **/
+    let pointArray = [];
+    const currentTime = new Date();
+    for (let i = 0; i < 100; i++) {
+      const measurement = "solar";
+      const amount = Math.floor(Math.random() * 1000);
+      const timestamp = new Date(currentTime - i * 60000);
+
+      const point = new Point(measurement)
+        .floatField("amount", amount)
+        .timestamp(timestamp);
+      console.log(`${point}`);
+      pointArray.push(point);
+    }
+    writeApi.writePoints(pointArray);
+
+    /**
+     * Flush pending writes and close writeApi.
+     **/
+    writeApi.close().then(() => {
+      console.log("WRITE FINISHED");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("\nError during deletion");
+  });
