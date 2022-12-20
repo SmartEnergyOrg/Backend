9a6eb745ac8bb2986fdc9ad1b0bc80c6fbfd6214
Joost <ja.vandam3@student.avans.nl>
2022-12-20 11:10:22 +0100

    removed outdated api endpoints

--- src/controllers/powersources/battery.controller.js
+++
@@ -1,8 +0,0 @@
-const batteryService = require("../../services/powersources/battery.service");
-
-const getCompleteTimeline = async (req, res) => {
-  const completeTimeline = await batteryService.getCompleteTimeline();
-  res.status(200).json(completeTimeline);
-};
-
-module.exports = { getCompleteTimeline };
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
 
 const dashboardConfigRoutes = require("./dashboardconfig/dashboard.routing");
-const powerSourcesRoutes = require("./powersources/powersources.routes");
 const widgetRoutes = require("./widgets/widgets.routes");
 const widgetConfigRoutes = require("./dashboardconfig/widget.routing");
 
-router.use("/powersources", powerSourcesRoutes);
 router.use("/widgets", widgetRoutes);
 router.use("/dashboards", dashboardConfigRoutes);
 router.use("/widgetsconfig", widgetConfigRoutes);
